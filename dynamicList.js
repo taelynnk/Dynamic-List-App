fb932fe622ab8050ebca0b52b396e06dfc737feb
Taelynn Knutson <taelynnknutson@Taelynns-MacBook-Pro.local>
2018-04-01 19:01:33 -0700

    i tried

--- dynamicList.js
+++ dynamicList.js
@@ -1,3 +1,20 @@
-function createListItem();
-var ul = document.getElementsByTagName('ul')[0];
-var newItem = document.createElement('li');
+var button = document.getElementById('addFirstItem');
+
+function addFirstItem() {
+    var ul = document.getElementsByTagName('ul')[0];
+    var newItem = document.createElement('li');
+    var input = newItem.value;
+
+    ul.appendChild(input);
+
+
+}
+
+button.addEventListener('click', addFirstItem);
+
+var button2 = document.getElementById('addOtherItems');
+
+function addOtherItems() {
+
+
+}
